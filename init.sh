df38e0156474673ed5e9baf95162ac0e558c18b0
tslater <toby@tgs.onl>
2023-11-26 17:59:51 +0000

    Add ly config and hard mv to init.sh

--- init.sh
+++ init.sh
@@ -44,3 +44,5 @@ chsh -s /bin/zsh
 # Install zap
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
+# ly config
+sudo mv $HOME/.cache/repos/dotfiles/etc/ly/config.ini /etc/ly/config.ini
