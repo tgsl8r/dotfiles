fc53253780e01d0604b843e60dd8d3583016bf06
tslater <toby@tgs.onl>
2023-12-02 16:47:06 +0000

    Add catppuccin colors to qtile dir and set bar and border colors

--- qtile/.config/qtile/layouts.py
+++ qtile/.config/qtile/layouts.py
@@ -2,14 +2,15 @@
 
 from libqtile import layout
 from libqtile.config import Match
-from colors import colors
+from catppuccin import colors
 
 # Default style
-layout_theme = {"border_width": 3,
-                "margin": 0,
-                "border_focus": colors['neutral_green'],
-                "border_normal": colors['dark1']
-                }
+layout_theme = {
+    "border_width": 3,
+    "margin": 0,
+    "border_focus": colors["mauve"],
+    "border_normal": colors["surface1"],
+}
 
 layouts = [
     layout.Columns(**layout_theme),
@@ -24,35 +25,34 @@ layouts = [
     # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
-
 ]
 
 floating_layout = layout.Floating(
     **layout_theme,
     float_rules=[
         # *layout.Floating.default_float_rules,
-        Match(title='Quit and close tabs?'),
-        Match(wm_type='utility'),
-        Match(wm_type='notification'),
-        Match(wm_type='toolbar'),
-        Match(wm_type='splash'),
-        Match(wm_type='dialog'),
-        Match(wm_class='gimp-2.99'),
-        Match(wm_class='file_progress'),
-        Match(wm_class='confirm'),
-        Match(wm_class='dialog'),
-        Match(wm_class='download'),
-        Match(wm_class='error'),
-        Match(wm_class='notification'),
-        Match(wm_class='splash'),
-        Match(wm_class='toolbar'),
-        Match(title='About LibreOffice'),
-        Match(wm_class='confirmreset'),  # gitk
-        Match(wm_class='makebranch'),  # gitk
-        Match(wm_class='maketag'),  # gitk
-        Match(wm_class='ssh-askpass'),  # ssh-askpass
-        Match(title='branchdialog'),  # gitk
-        Match(wm_class='pinentry-qt'),  # GPG key password entry
+        Match(title="Quit and close tabs?"),
+        Match(wm_type="utility"),
+        Match(wm_type="notification"),
+        Match(wm_type="toolbar"),
+        Match(wm_type="splash"),
+        Match(wm_type="dialog"),
+        Match(wm_class="gimp-2.99"),
+        Match(wm_class="file_progress"),
+        Match(wm_class="confirm"),
+        Match(wm_class="dialog"),
+        Match(wm_class="download"),
+        Match(wm_class="error"),
+        Match(wm_class="notification"),
+        Match(wm_class="splash"),
+        Match(wm_class="toolbar"),
+        Match(title="About LibreOffice"),
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(wm_class="pinentry-qt"),  # GPG key password entry
         # Match(func=lambda c: c.is_transient_for()),  # automatically float a window if it has a parent
     ],
 )
--- qtile/.config/qtile/widgets.py
+++ qtile/.config/qtile/widgets.py
@@ -6,14 +6,11 @@ import subprocess
 from libqtile import qtile
 from libqtile import widget
 from libqtile.lazy import lazy
-from colors import colors
+from catppuccin import colors
 
 
 widget_defaults = dict(
-    font='FiraCode Nerd Font',
-    fontsize='14',
-    padding=2,
-    foreground=colors['light0']
+    font="FiraCode Nerd Font", fontsize="14", padding=2, foreground=colors["flamingo"]
 )
 
 extension_defaults = widget_defaults.copy()
@@ -22,17 +19,17 @@ primary_widgets = [
     widget.Spacer(length=10),
     widget.GroupBox(
         padding=0,
-        active=colors['light0'],
+        active=colors["flamingo"],
         borderwidth=3,
-        inactive=colors['light4'],
-        this_current_screen_border=colors['neutral_green'],
-        this_screen_border=colors['neutral_green'],
-        other_screen_border='#00000000',
-        other_current_screen_border='#00000000',
-        font='FiraCode Nerd Font',
+        inactive=colors["surface2"],
+        this_current_screen_border=colors["mauve"],
+        this_screen_border=colors["mauve"],
+        other_screen_border=colors["crust"],
+        other_current_screen_border=colors["crust"],
+        font="FiraCode Nerd Font",
         fontsize=14,
-        highlight_method='line',
-        highlight_color=['00000000', '00000000']
+        highlight_method="line",
+        highlight_color=["00000000", "00000000"],
     ),
     widget.CurrentLayoutIcon(scale=0.5, **widget_defaults),
     # widget.CurrentLayout(**widget_defaults),
@@ -43,79 +40,114 @@ primary_widgets = [
     widget.CheckUpdates(
         **widget_defaults,
         update_interval=600,
-        distro='Arch_paru',
-        custom_command='~/.local/bin/bar/updates',
-        display_format=' {updates}',
-        colour_have_updates=colors['neutral_green'],
-        execute='alacritty zsh -c "pikaur -Syu"'
+        distro="Arch_paru",
+        custom_command="~/.local/bin/bar/updates",
+        display_format=" {updates}",
+        colour_have_updates=colors["mauve"],
+        execute='alacritty zsh -c "pikaur -Syu"',
     ),
     widget.Spacer(length=5),
-#    widget.GenPollText(
-#        update_interval=1, 
-#        **widget_defaults, 
-#        func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/bar/idleinhibit")).decode(), 
-#        mouse_callbacks=
-#        {'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/idleinhibit toggle"), shell=True)}
-#        ),
+    #    widget.GenPollText(
+    #        update_interval=1,
+    #        **widget_defaults,
+    #        func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/bar/idleinhibit")).decode(),
+    #        mouse_callbacks=
+    #        {'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/idleinhibit toggle"), shell=True)}
+    #        ),
     widget.Spacer(length=5),
-    widget.KeyboardLayout(configured_keyboards=['gb', 'us']),
+    widget.KeyboardLayout(configured_keyboards=["gb", "us"]),
     widget.Spacer(length=5),
     widget.GenPollText(
-        update_interval=1, 
-        **widget_defaults, 
-        func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/bar/brightness")).decode(), 
+        update_interval=1,
+        **widget_defaults,
+        func=lambda: subprocess.check_output(
+            os.path.expanduser("~/.local/bin/bar/brightness")
+        ).decode(),
         mouse_callbacks={
-            'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/brightness down"), shell=True), 
-            'Button3': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/brightness up"), shell=True)}),
+            "Button1": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/brightness down"), shell=True
+            ),
+            "Button3": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/brightness up"), shell=True
+            ),
+        },
+    ),
     widget.Spacer(length=5),
     widget.GenPollText(
-        update_interval=1, 
-        **widget_defaults, 
-        func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/bar/volume")).decode(), 
+        update_interval=1,
+        **widget_defaults,
+        func=lambda: subprocess.check_output(
+            os.path.expanduser("~/.local/bin/bar/volume")
+        ).decode(),
         mouse_callbacks={
-            'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/volume down"), shell=True), 
-            'Button2': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/volume mute"), shell=True), 
-            'Button3': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/volume up"), shell=True)}),
+            "Button1": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/volume down"), shell=True
+            ),
+            "Button2": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/volume mute"), shell=True
+            ),
+            "Button3": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/volume up"), shell=True
+            ),
+        },
+    ),
     widget.Spacer(length=5),
     widget.GenPollText(
-            update_interval=1, 
-            **widget_defaults, 
-            func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/bar/network")).decode(), 
-            mouse_callbacks={
-                'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/network ShowInfo"), shell=True), 
-                'Button3': lambda: lazy.spawn("alacritty -e nmtui", shell=True)}),
-    widget.Spacer(length=5),
-    widget.GenPollText(
-        update_interval=1, 
-        **widget_defaults, 
-        func=lambda: subprocess.check_output(os.path.expanduser("~/.local/bin/bar/battery.py")).decode(), 
+        update_interval=1,
+        **widget_defaults,
+        func=lambda: subprocess.check_output(
+            os.path.expanduser("~/.local/bin/bar/network")
+        ).decode(),
         mouse_callbacks={
-            'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/bar/battery.py --c left-click"), shell=True)}),
+            "Button1": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/network ShowInfo"), shell=True
+            ),
+            "Button3": lambda: lazy.spawn("alacritty -e nmtui", shell=True),
+        },
+    ),
+    widget.Spacer(length=5),
+    widget.GenPollText(
+        update_interval=1,
+        **widget_defaults,
+        func=lambda: subprocess.check_output(
+            os.path.expanduser("~/.local/bin/bar/battery.py")
+        ).decode(),
+        mouse_callbacks={
+            "Button1": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/bar/battery.py --c left-click"),
+                shell=True,
+            )
+        },
+    ),
     widget.Spacer(length=5),
     widget.Clock(
         format="| %Y-%m-%d %a %I:%M %p",
         mouse_callbacks={
-            'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/calendar show"), shell=True),
-            'Button3': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/calendar edit"), shell=True)
-            }
-        ),
+            "Button1": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/calendar show"), shell=True
+            ),
+            "Button3": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/calendar edit"), shell=True
+            ),
+        },
+    ),
 ]
 
 secondary_widgets = [
     widget.Spacer(length=10),
     widget.GroupBox(
         padding=0,
-        active=colors['light0'],
+        active=colors["rosewater"],
         borderwidth=3,
-        inactive=colors['light4'],
-        this_current_screen_border=colors['neutral_green'],
-        this_screen_border=colors['neutral_green'],
-        other_screen_border='#00000000',
-        other_current_screen_border='#00000000',
-        font='GoMono Nerd Font',
+        inactive=colors["flamingo"],
+        this_current_screen_border=colors["mauve"],
+        this_screen_border=colors["mauve"],
+        other_screen_border=colors["crust"],
+        other_current_screen_border=colors["crust"],
+        font="FiraCode Nerd Font",
         fontsize=12,
-        highlight_method='line',
-        highlight_color=['00000000', '00000000']
+        highlight_method="line",
+        highlight_color=["00000000", "00000000"],
     ),
     widget.CurrentLayoutIcon(scale=0.6, **widget_defaults),
     widget.CurrentLayout(**widget_defaults),
@@ -123,8 +155,12 @@ secondary_widgets = [
     widget.Clock(
         format="| %Y-%m-%d %a %I:%M %p",
         mouse_callbacks={
-            'Button1': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/calendar.sh show"), shell=True),
-            'Button3': lambda: lazy.spawn(os.path.expanduser("~/.local/bin/calendar.sh edit"), shell=True)
-            }
-        ),
+            "Button1": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/calendar.sh show"), shell=True
+            ),
+            "Button3": lambda: lazy.spawn(
+                os.path.expanduser("~/.local/bin/calendar.sh edit"), shell=True
+            ),
+        },
+    ),
 ]
